b8c2cab94174a1777f4125134f4e9c7a0f14c9c2
ecoronado89 <coreddin@gmail.com>
2025-02-04 20:12:22 -0600

    Improving code reusability

--- features/stepDefinitions/steps.js
+++ features/stepDefinitions/steps.js
@@ -1,67 +1,29 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-let cucumbers = 0;
-let carrots = 0;
-let salads = 0;
+const { Given, When, Then } = require("@cucumber/cucumber");
+const assert = require("assert");
 
-// Given steps
-Given('I have {int} cucumbers', function (count) {
-  cucumbers = count;
+
+let inventory = {};
+
+Given(/^I have (\d+) (cucumbers|carrots)$/, function (quantity, item) {
+    inventory[item] = parseInt(quantity);
 });
 
-Given('I have {int} carrots', function (count) {
-  carrots = count;
+When(/^I eat (\d+) (cucumbers|carrots)$/, function (quantity, item) {
+    inventory[item] = Math.max(0, inventory[item] - parseInt(quantity));
 });
 
-Given('I have {int} cucumbers and {int} carrots', function (cucumberCount, carrotCount) {
-  cucumbers = cucumberCount;
-  carrots = carrotCount;
+When(/^I make a salad with (\d+) cucumbers and (\d+) carrots$/, function (cucumber, carrots) {
+    if ( inventory['cucumbers'] >= cucumber && inventory['carrots'] >= carrots) {
+        inventory['cucumbers'] -= cucumber;
+        inventory['carrots'] -= carrots;
+        inventory['salad'] = (inventory['salad'] || 0) + 1;
+    }
 });
 
-// When steps
-When('I eat {int} cucumbers', function (count) {
-  cucumbers -= count;
+Then(/^I have (\d+) (cucumbers|carrots) in my inventory$/, function (quantity, item) {
+    assert.strictEqual(inventory[item], parseInt(quantity));
 });
 
-When('I eat {int} carrots', function (count) {
-  carrots -= count;
-});
-
-When('I make a salad with {int} cucumbers and {int} carrots', function (cucumberCount, carrotCount) {
-  cucumbers -= cucumberCount;
-  carrots -= carrotCount;
-  salads += 1;
-});
-
-// Then steps
-Then('I have {int} cucumber', function (count) {
-  if (cucumbers !== count) {
-    throw new Error(`Expected ${count} cucumbers, but got ${cucumbers}`);
-  }
-});
-
-Then('I have {int} carrots', function (count) {
-  if (carrots !== count) {
-    throw new Error(`Expected ${count} carrots, but got ${carrots}`);
-  }
-});
-
-Then('I should have 0 carrots', function () {
-  if (carrots < 0) {
-    carrots = 0; // You can't have negative carrots
-  }
-  if (carrots !== 0) {
-    throw new Error(`Expected 0 carrots, but got ${carrots}`);
-  }
-});
-
-Then('I have {int} cucumbers', function (count) {
-  if (cucumbers !== count) {
-    throw new Error(`Expected ${count} cucumbers, but got ${cucumbers}`);
-  }
-});
-
-Then('I have {int} salads', function (count) {
-  if (salads !== count) {
-    throw new Error(`Expected ${count} salads, but got ${salads}`);
-  }
-});
+Then(/^I have (\d+) salad$/, function(quantity) {
+    assert.strictEqual(inventory['salad'], parseInt(quantity));
+})
